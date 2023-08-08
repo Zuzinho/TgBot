3d0337a1989b0165874867216d28e5a818cd7e78
Никита Зозуля <nik.zozulya.04@mail.ru>
2023-08-08 14:21:47 +0300

    Func for getting token fron config.json file

---
+++ token/token.go
@@ -0,0 +1,27 @@
+package token
+
+import (
+	"ZuzinhoBot/errlog"
+	"encoding/json"
+	"io"
+	"os"
+)
+
+type config struct {
+	BotTgToken string `json:"tg_bot_token"`
+}
+
+func Token() string {
+	f, err := os.Open("config.json")
+	errlog.LogOnErr(err)
+
+	jsonStr, err := io.ReadAll(f)
+	errlog.LogOnErr(err)
+
+	conf := config{}
+
+	err = json.Unmarshal(jsonStr, &conf)
+	errlog.LogOnErr(err)
+
+	return conf.BotTgToken
+}
