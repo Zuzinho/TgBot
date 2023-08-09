5026d63d4a70bd699e3c9ab4caa0408b3b9a7bc6
Никита Зозуля <nik.zozulya.04@mail.ru>
2023-08-09 23:14:32 +0300

    Modified func Handle to method for structs Handler, implemented Handler interface

--- markup/usermarkup/buttonhandler/smtsleeping/handler.go
+++ markup/usermarkup/buttonhandler/smtsleeping/handler.go
@@ -2,15 +2,18 @@ package smtsleeping
 
 import (
 	"ZuzinhoBot/generator/sleepingstory"
-	"ZuzinhoBot/markup/usermarkup/buttonhandler"
+	"ZuzinhoBot/markup/usermarkup/buttonhandler/readerhandler"
 	api "github.com/go-telegram-bot-api/telegram-bot-api"
 )
 
-func Handle(chatId int64) ([]*api.MessageConfig, error) {
+type Handler struct {
+}
+
+func (*Handler) Handle(chatId int64) ([]*api.MessageConfig, error) {
 	rd, err := sleepingstory.Story()
 	if err != nil {
 		return nil, err
 	}
 
-	return buttonhandler.HandleReader(rd, chatId)
+	return readerhandler.HandleReader(rd, chatId)
 }
--- markup/usermarkup/buttonhandler/smttelling/handler.go
+++ markup/usermarkup/buttonhandler/smttelling/handler.go
@@ -2,15 +2,18 @@ package smttelling
 
 import (
 	"ZuzinhoBot/generator/phrase"
-	"ZuzinhoBot/markup/usermarkup/buttonhandler"
+	"ZuzinhoBot/markup/usermarkup/buttonhandler/readerhandler"
 	api "github.com/go-telegram-bot-api/telegram-bot-api"
 )
 
-func Handle(chatId int64) ([]*api.MessageConfig, error) {
+type Handler struct {
+}
+
+func (*Handler) Handle(chatId int64) ([]*api.MessageConfig, error) {
 	rd, err := phrase.Phrase()
 	if err != nil {
 		return nil, err
 	}
 
-	return buttonhandler.HandleReader(rd, chatId)
+	return readerhandler.HandleReader(rd, chatId)
 }
