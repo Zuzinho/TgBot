2ac6e82158d7da840604abd0f9a23a24f1b96a65
Никита Зозуля <nik.zozulya.04@mail.ru>
2023-08-09 12:55:15 +0300

    Added logs

--- main.go
+++ main.go
@@ -29,38 +29,47 @@ func main() {
 
 	for update := range updates {
 		if update.Message != nil {
-			switch users.GetRole(update.Message.Chat.FirstName) {
+			sentMsg := update.Message
+			firstName := sentMsg.Chat.FirstName
+			log.Printf("User %s sent message with text '%s'", firstName, sentMsg.Text)
+			switch users.GetRole(firstName) {
 			case users.User:
+				log.Printf("User %s has role 'User'", firstName)
 				if !isStart {
-					msg := api.NewMessage(update.Message.Chat.ID, "Привет")
+					msg := api.NewMessage(sentMsg.Chat.ID, "Привет")
 					msg.ReplyMarkup = usermarkup.Default()
 
 					_, err = bot.Send(msg)
 					errlog.LogOnErr(err)
 					isStart = true
 				} else {
-					msg := api.NewMessage(update.Message.Chat.ID, "Нажми на какую-нибудь кнопку")
+					msg := api.NewMessage(sentMsg.Chat.ID, "Нажми на какую-нибудь кнопку")
 
 					_, err = bot.Send(msg)
 					errlog.LogOnErr(err)
 				}
 			case users.Admin:
-				msg := api.NewMessage(update.Message.Chat.ID, "Здраствуйте, хозяин")
+				log.Printf("User %s has role 'Admin'", firstName)
+				msg := api.NewMessage(sentMsg.Chat.ID, "Здраствуйте, хозяин")
 				_, err = bot.Send(msg)
 				errlog.LogOnErr(err)
 			}
 		} else if update.CallbackQuery != nil {
-			switch users.GetRole(update.CallbackQuery.Message.Chat.FirstName) {
+			sentQuery := update.CallbackQuery
+			firstName := sentQuery.Message.Chat.FirstName
+			log.Printf("User %s sent query with text '%s'", firstName, sentQuery.Data)
+			switch users.GetRole(sentQuery.Message.Chat.FirstName) {
 			case users.User:
-				msgSlice, err := usermarkup.HandleDefault(update, bot)
+				msgSlice, err := usermarkup.HandleDefault(sentQuery, bot)
 				errlog.LogOnErr(err)
 
 				for _, msg := range msgSlice {
 					_, err = bot.Send(*msg)
 					errlog.LogOnErr(err)
+					log.Printf("Message '%s' was sent to user %s", msg.Text, firstName)
 					time.Sleep(time.Second)
 				}
-				lastMsg := api.NewMessage(update.CallbackQuery.Message.Chat.ID, "Еще?")
+				lastMsg := api.NewMessage(sentQuery.Message.Chat.ID, "Еще?")
 				lastMsg.ReplyMarkup = usermarkup.Default()
 				_, err = bot.Send(lastMsg)
 				errlog.LogOnErr(err)
