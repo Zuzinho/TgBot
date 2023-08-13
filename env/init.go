78b07bfaf92b41f1ccf9a03bacffd7f050c226b7
Никита Зозуля <nik.zozulya.04@mail.ru>
2023-08-13 22:19:12 +0300

    Added func init and Global vars for comfortable using secret data

---
+++ env/init.go
@@ -0,0 +1,36 @@
+package env
+
+import (
+	"github.com/jackc/pgx"
+	"github.com/joho/godotenv"
+	"log"
+	"os"
+)
+
+var (
+	Token      string
+	ConnConfig pgx.ConnConfig
+)
+
+func init() {
+	if err := godotenv.Load(); err != nil {
+		log.Fatal(".env file not found")
+	}
+
+	var exists bool
+	var err error
+
+	Token, exists = os.LookupEnv("TG_BOT_TOKEN")
+	if !exists {
+		log.Fatal("TgBot token not found")
+	}
+
+	connString, exists := os.LookupEnv("DB_CONN_STRING")
+	if !exists {
+		log.Fatal("Connection string not found")
+	}
+	ConnConfig, err = pgx.ParseConnectionString(connString)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
