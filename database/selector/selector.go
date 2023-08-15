04c0b4bb3b7cb8f8b4f4c6cab0acaa6404ca87cc
Никита Зозуля <nik.zozulya.04@mail.ru>
2023-08-16 02:45:43 +0300

    Changed parametr type to database.Table and checking validness of parametrs value

--- database/selector/selector.go
+++ database/selector/selector.go
@@ -1,12 +1,19 @@
 package selector
 
 import (
+	"ZuzinhoBot/database"
 	"ZuzinhoBot/env"
 	"github.com/jackc/pgx"
 	"log"
 )
 
-func Select(table string) (value string, err error) {
+func Select(table database.Table) (value string, err error) {
+	if !table.IsValid() {
+		return "", database.NoTableError{
+			Table: table,
+		}
+	}
+
 	conn, err := pgx.Connect(env.ConnConfig)
 	if err != nil {
 		return "", err
@@ -15,18 +22,18 @@ func Select(table string) (value string, err error) {
 
 	var id int
 
-	err = conn.QueryRow("select id, content from "+table+" where is_read = false order by RANDOM() limit 1").Scan(&id, &value)
+	err = conn.QueryRow("select id, content from "+string(table)+" where is_read = false order by RANDOM() limit 1").Scan(&id, &value)
 	if err == pgx.ErrNoRows {
 		log.Printf("Read all story from table %s", table)
 
-		err = conn.QueryRow("select content from " + table + " order by RANDOM() limit 1").Scan(&value)
+		err = conn.QueryRow("select content from " + string(table) + " order by RANDOM() limit 1").Scan(&value)
 		if err != nil {
 			return "", err
 		}
 	} else if err != nil {
 		return "", err
 	} else {
-		_, err = conn.Exec("update "+table+" set is_read = true where id = $1", id)
+		_, err = conn.Exec("update "+string(table)+" set is_read = true where id = $1", id)
 		if err != nil {
 			return "", err
 		}
