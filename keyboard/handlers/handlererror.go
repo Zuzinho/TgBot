e769e9aef1a33024c424d3beb2d00033f852ce9c
Никита Зозуля <nik.zozulya.04@mail.ru>
2023-08-18 01:41:53 +0300

    Created error type for unable of not existed data handlers

---
+++ keyboard/handlers/handlererror.go
@@ -0,0 +1,21 @@
+package handlers
+
+import (
+	"ZuzinhoBot/keyboard/values"
+	"fmt"
+)
+
+type NoHandlerError struct {
+	error
+	value values.KeyboardValue
+}
+
+func NewNoHandlerError(value values.KeyboardValue) NoHandlerError {
+	return NoHandlerError{
+		value: value,
+	}
+}
+
+func (err NoHandlerError) String() string {
+	return fmt.Sprintf("No handler for such keyboard value as '%d' or its one is unable", err.value)
+}
