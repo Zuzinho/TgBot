a1d8f864f649f182633346eafee4acee096465a1
Никита Зозуля <nik.zozulya.04@mail.ru>
2023-08-27 11:23:58 +0300

    Created func for getting []*tgbotapi.MessageConfog for admin and for specific role

--- keyboard/handlers/mainhandler.go
+++ keyboard/handlers/mainhandler.go
@@ -3,6 +3,7 @@ package handlers
 import (
 	"ZuzinhoBot/botapi/types"
 	"ZuzinhoBot/keyboard/values"
+	"ZuzinhoBot/users"
 )
 
 func HandleUserKeyboard(value values.KeyboardValue, chatId int64) (types.Messages, error) {
@@ -12,4 +13,24 @@ func HandleUserKeyboard(value values.KeyboardValue, chatId int64) (types.Message
 	}
 
 	return HandleReader(rd, chatId)
+}
+
+func HandleAdminKeyboard(value values.KeyboardValue, chatId int64) (types.Messages, error) {
+	rd, err := AdminHandler(value)
+	if err != nil {
+		return nil, err
+	}
+
+	return HandleReader(rd, chatId)
+}
+
+func HandleKeyboard(value values.KeyboardValue, chatId int64, role users.Role) (types.Messages, error) {
+	switch role {
+	case users.Admin:
+		return HandleAdminKeyboard(value, chatId)
+	case users.User:
+		return HandleUserKeyboard(value, chatId)
+	default:
+		return nil, NewInAccessedHandlerError(value)
+	}
 }
