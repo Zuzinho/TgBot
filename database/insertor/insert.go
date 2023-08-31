bba3197b56796073fb7d8ead19a2b8427d2177c1
Никита Зозуля <nik.zozulya.04@mail.ru>
2023-08-31 16:15:16 +0300

    Added funcs for inserting unwanted user to data base and messages to users_statistic table

--- database/insertor/insert.go
+++ database/insertor/insert.go
@@ -1,8 +1,10 @@
 package insertor
 
 import (
+	"ZuzinhoBot/database/othertype/statistictype"
 	"ZuzinhoBot/env"
 	"github.com/jackc/pgx"
+	"time"
 )
 
 func InsertUnwantedUser(chatId int64, userName string) (err error) {
@@ -13,6 +15,19 @@ func InsertUnwantedUser(chatId int64, userName string) (err error) {
 	defer conn.Close()
 
 	_, err = conn.Exec("insert into users (id, name, role) values (&1, &2, 1)", chatId, userName)
+
+	return
+}
+
+func InsertUserMessage(userId int64, dataType statistictype.DataType, date time.Time) (err error) {
+	conn, err := pgx.Connect(env.ConnConfig)
+	if err != nil {
+		return
+	}
+	defer conn.Close()
+
+	_, err = conn.Exec("insert into users_statistic (user_id, data_type, date) values ($1, $2, $3)",
+		userId, dataType, date.Format("2006-01-02"))
 
 	return
 }
